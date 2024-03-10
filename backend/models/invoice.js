d082394d72fe14f36cd86700a6ca0d7b03464aa3
Taherpatrawala <109360109+Taherpatrawala@users.noreply.github.com>
2024-03-10 21:58:08 +0530

    Created invoice schema

---
+++ backend/models/invoice.js
@@ -0,0 +1,51 @@
+import mongoose from "mongoose";
+
+const InvoiceSchema = new mongoose.Schema({
+  invoiceNumber: {
+    type: String,
+    required: true,
+  },
+  invoiceDate: {
+    type: Date,
+    required: true,
+  },
+  items: [
+    {
+      name: {
+        type: String,
+        required: true,
+      },
+      quantity: {
+        type: Number,
+        required: true,
+      },
+      price: {
+        type: Number,
+        required: true,
+      },
+    },
+  ],
+  subtotal: {
+    type: Number,
+    required: true,
+  },
+  discount: {
+    type: Number,
+    required: true,
+  },
+  gst: {
+    type: Number,
+    required: true,
+  },
+  grandTotal: {
+    type: Number,
+    required: true,
+  },
+
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+});
+
+export default mongoose.model("Invoice", InvoiceSchema);
